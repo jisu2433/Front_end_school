245d08be1dfbcf839e3a56df9bc126b33d392a1f
Jisu Kim <93359953+jisu2433@users.noreply.github.com>
2021-12-01 21:42:09 +0900

    JS100제-1일차

---
+++ JS_100/003.js
@@ -0,0 +1,15 @@
+// 문제3 : 변수의 타입
+// 다음 출력 값으로 올바른 것은?
+
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+
+1)  undefined
+2)  string
+3)  number
+4)  object
+
+// 정답 : 4번
+// 자바스크립트 변수의 타입
+// 원시 타입 : number, string, boolean, undefined, null
+// 참조 타입 : object
