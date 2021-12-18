3f698f4ec754cb831e2d1064f4550667e1a932e0
Jisu Kim <93359953+jisu2433@users.noreply.github.com>
2021-12-18 23:01:21 +0900

    JS100제-DAY12

---
+++ JS_100/036.js
@@ -0,0 +1,9 @@
+// 문제36 : 구구단 출력하기
+//1~9까지의 숫자 중 하나를 입력하면 그 단의 구구단 결과를 한 줄에 출력하는 프로그램을 작성하세요.
+
+const num = prompt('1 ~ 9까지의 숫자 중 하나를 입력하세요.')
+let result = '';
+for (let i = 1; i <= 9; i++) {
+    result = result + (num*i) + ' ';  
+    }
+    console.log(result);
